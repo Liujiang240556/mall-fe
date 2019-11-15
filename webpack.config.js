44b3f721531e816202763679bbc872c699782894
lj240556 <1226263601@qq.com>
2019-11-15 15:19:17 +0800

    ss

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ var config = {
         //publicPath  : 'dev' === WEBPACK_ENV ? (__dirname + '/dist/'): '//s.happymmall.com/mall-fe/dist/',
 
         //线上写法
-        publicPath  :'dev' === WEBPACK_ENV ?'/dist/':'http://106.75.172.117:8163/mall-fe/dist/',
+     //   publicPath  :'dev' === WEBPACK_ENV ?'/dist/':'//106.75.172.117:8163/mall-fe/dist/',
         filename    : 'js/[name].js'
     },
     externals : {
